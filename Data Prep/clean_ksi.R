0d1b01695eb291f0b7d8cba7d1c27e363057aa41
Angela Kang <angelakang50@gmail.com>
2019-06-15 15:55:04 -0400

    Update clean_ksi.R

--- Data Prep/clean_ksi.R
+++ Data Prep/clean_ksi.R
@@ -3,11 +3,21 @@
 ### seriously injured in 2008-2018 in Toronto.
 ### For more information refer to: http://data.torontopolice.on.ca/datasets/ksi
 
+library(tidyverse)
 library(httr)
 library(jsonlite)
 
+url <- "https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/KSI/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+
+get_ksi <- GET(url)
+pages <- get_ksi$total_pages
+
 base_url <- "https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/KSI/FeatureServer/0/query?"
-end_url <- "where=1%3D1&outFields=*&outSR=4326&f=json"
-ksi_json <- fromJSON(content(GET(url), "text"), flatten = TRUE)
+end_url <- "&outFields=*&outSR=4326&f=json"
+
+ksi_json <- map(2008:2018, 
+                function(x) fromJSON(content(GET(paste0(base_url, "where=YEAR=", x, end_url)), 
+                                             "text"), 
+                                     flatten = TRUE))
 
 ksi <- as.data.frame(ksi_json)
