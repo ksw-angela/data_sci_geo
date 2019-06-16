314f6a9a12badf3088150dc06deaf5c44b93b248
Angela Kang <angelakang50@gmail.com>
2019-06-16 01:54:09 -0400

    Update clean_ksi.R

--- Data Prep/clean_ksi.R
+++ Data Prep/clean_ksi.R
@@ -6,18 +6,40 @@
 library(tidyverse)
 library(httr)
 library(jsonlite)
-
-url <- "https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/KSI/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
-
-get_ksi <- GET(url)
-pages <- get_ksi$total_pages
+library(rvest)
 
 base_url <- "https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/KSI/FeatureServer/0/query?"
 end_url <- "&outFields=*&outSR=4326&f=json"
 
+# Toronto Police limits queries to 2000 entries so get one year at a time
 ksi_json <- map(2008:2018, 
-                function(x) fromJSON(content(GET(paste0(base_url, "where=YEAR=", x, end_url)), 
-                                             "text"), 
-                                     flatten = TRUE))
+                ~fromJSON(content(GET(paste0(base_url, "where=YEAR=", .x, end_url)), "text"), 
+                          flatten = TRUE))
 
-ksi <- as.data.frame(ksi_json)
+# Only require the KSI dataset
+ksi_df <- map_df(ksi_json, ~bind_rows(.x[["features"]]))
+
+rm(ksi_json)
+
+# Clean final KSI dataset
+# 1. Create a single date column
+# 2. Attach the ward and hood name
+# 3. Remove the geometry columns
+# 4. Remove attributes. from column names
+
+# Ward information
+ward_info <- read_html("https://www.toronto.ca/city-government/data-research-maps/neighbourhoods-communities/ward-profiles/") %>%
+  html_node("#js_map--data") %>%
+  html_table() %>%
+  select(`Ward Number`, `Ward Name`) %>%
+  mutate(`Ward Number` = as.character(`Ward Number`))
+
+# Neighborhood information
+hood_info <- fread("https://www.toronto.ca/ext/open_data/catalog/data_set_files/2016_neighbourhood_profiles.csv",
+                   nrows = 1) %>%
+  select(-Category, -Topic, -`Data Source`, -Characteristic, -`City of Toronto`) %>%
+  gather(`Hood Name`, `Hood Number`) %>%
+  mutate(`Hood Number` = as.character(`Hood Number`))
+
+ksi <- ksi_df %>%
+  select(-starts_with("geometry"))
