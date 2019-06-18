75c7ed56bbd42bf5bf9f859d06f7d870c744f201
Angela Kang <angelakang50@gmail.com>
2019-06-18 16:48:35 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,43 +1,45 @@
 library(shiny)
+library(shinythemes)
 
 shinyUI(fluidPage(
+  theme = shinytheme("simplex"),
   
-  h2("Road Accidents in Toronto"),
+  h2("Road Collisions in Toronto"),
   
   sidebarLayout(
     sidebarPanel(
       actionButton("action", "Create Map"),
       br(), br(), 
       
-      dateRangeInput("acc_date", label = "Date of accident",
+      dateRangeInput("acc_date", label = "Date of Accident",
                      start = min(as.Date(accidents$Date)), end = max(as.Date(accidents$Date)),
                      min = min(as.Date(accidents$Date)), max = max(as.Date(accidents$Date))),
       
-      checkboxGroupInput("fatal", label = "Accident Fatality",
-                         choices = c("Fatal", "Non-Fatal Injury"),
-                         selected = c("Fatal", "Non-Fatal Injury")),
+      checkboxGroupInput("fatal", label = "Fatal Accident",
+                         choices = c("Yes", "No"),
+                         selected = c("Yes", "No")),
       
-      selectInput("auto_type", label = "Other Vehicles Involved",
+      selectInput("auto_type", label = "Involved Vehicles",
                   choices = c("Automobile", "Pedestrian", "Bicycle", "Motorcycle", 
-                              "Truck", "Emergency Vehicle", "All"),
-                  selected = c("All")),
+                              "Truck", "Emergency Vehicle"),
+                  selected = c("Automobile"), multiple = TRUE),
       
       selectInput("precip", label = "Weather Condition",
-                  choices = c("Clear", "Precipitated", "All"), 
-                  selected = c("All")),
+                  choices = c("Rain", "Snow"), 
+                  selected = NULL, multiple = TRUE),
       
       selectInput("visib", label = "Visibility",
-                  choices = c("Clear", "Not Clear", "All"),
-                  selected = c("All")),
+                  choices = c("Clear", "Not Clear"),
+                  selected = NULL, multiple = TRUE),
       
       selectInput("road_class", label = "Road Class",
-                  choices = c("Arterial", "Collector", "Expressway", "Local", ""),
-                  selected = c("")),
+                  choices = c("Arterial", "Collector", "Expressway", "Local", "Any"),
+                  selected = c("Any")),
       
       selectInput("traffic_ctrl", label = "Traffic Control",
                   choices = c("No Traffic Control", "Human Control", "Traffic Sign",
-                              "Pedestrian Crossing", ""),
-                  selected = c("")),
+                              "Pedestrian Crossing", "Any"),
+                  selected = c("Any")),
       
       sliderInput("acc_time", label = "Hour of accident",
                   min = 0, max = 24, step = 1,
