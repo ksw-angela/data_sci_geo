c637ac5f2de8eaf0d5a7bc58a0362a55310d5b78
Angela Kang <angelakang50@gmail.com>
2019-06-19 21:08:28 -0400

    updates date range for past 10 years
    the ksi api updates to only reflect collisions in past 10 years

--- clean_ksi.R
+++ clean_ksi.R
@@ -1,27 +1,41 @@
 ### The following script interacts with the Toronto Police Office's Killed or Seriously Injured
 ### (KSI) API; A dataset of all traffic collisions where a person was either killed or
-### seriously injured in 2008-2018 in Toronto.
+### seriously injured in the past 10 years inclusive in Toronto. At time of publishing this
+### was from 2008 - 2018.
 ### For more information refer to: http://data.torontopolice.on.ca/datasets/ksi
 
-library(tidyverse)
+library(dplyr)
+library(purrr)
+library(tidyr)
 library(httr)
 library(jsonlite)
-library(rvest)
-library(zoo)
 library(data.table)
 
 base_url <- "https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/KSI/FeatureServer/0/query?"
 end_url <- "&outFields=*&outSR=4326&f=json"
 
+# Find earliest collision KSI currently has record of
+earliest_url <- "where=1%3D1&outFields=*&orderByFields=YEAR&outSR=4326&f=json"
+earliest_json <- fromJSON(content(GET(paste0(base_url, earliest_url)), "text"), 
+                          flatten = T)
+earliest_date <- earliest_json[["features"]] %>%
+  summarize(Year = min(`attributes.YEAR`)) %>% 
+  as.numeric()
+
+# Find latest collision KSI currently has record of
+latest_date <- as.numeric(format(Sys.Date(), "%Y")) - 1
+
+rm(earliest_url, earliest_json)
+
 # Toronto Police limits queries to 2000 entries so get one year at a time
-ksi_json <- map(2008:2018, 
+ksi_json <- map(earliest_date:latest_date, 
                 ~fromJSON(content(GET(paste0(base_url, "where=YEAR=", .x, end_url)), "text"), 
                           flatten = TRUE))
 
 # Only require the KSI dataset
 ksi_df <- map_df(ksi_json, ~bind_rows(.x[["features"]]))
 
-rm(ksi_json, base_url, end_url)
+rm(ksi_json, base_url, end_url, earliest_date, latest_date)
 
 # Neighborhood information
 hood_info <- fread("https://www.toronto.ca/ext/open_data/catalog/data_set_files/2016_neighbourhood_profiles.csv",
