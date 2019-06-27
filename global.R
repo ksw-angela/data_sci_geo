927eacedeccf1fcf851da54a0468f1650ab70e94
Angela Kang <angelakang50@gmail.com>
2019-06-26 22:09:01 -0400

    Update global.R

--- global.R
+++ global.R
@@ -36,13 +36,20 @@ accidents <- ksi %>%
                                                 "Pending"), "Local",
                               if_else(ROAD_CLASS %in% c("Major Arterial",
                                                         "Minor Arterial"), "Arterial",
-                                      ROAD_CLASS)))
+                                      ROAD_CLASS)),
+         INVAGE = factor(INVAGE, levels = c("0 to 4", "5 to 9", "10 to 14",
+                                            "15 to 19", "20 to 24", "25 to 29", 
+                                            "30 to 34", "35 to 39", "40 to 44", 
+                                            "45 to 49", "50 to 54", "55 to 59",
+                                            "60 to 64", "65 to 69", "70 to 74",
+                                            "75 to 79", "80 to 84", "85 to 89", 
+                                            "90 to 94", "Over 95", "unknown")))
 
 # Number of parties involved in an accident as well as number of fatalities
 per_accident <- accidents %>%
   group_by(ACCNUM) %>%
   summarize(Involved = n(),
-            Fatalities = sum(ACCLASS == "Fatal"))
+            Fatalities = sum(INJURY == "Fatal"))
 
 accidents <- accidents %>%
   left_join(per_accident, by = c("ACCNUM" = "ACCNUM"))
